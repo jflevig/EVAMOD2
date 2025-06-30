228f5d0f1ddd33c112d46391b824a3496b216ad4
jflevi <jfelevi@gmail.com>
2025-06-30 17:21:29 -0400

    Consejos y formulario agregados

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
-/* $('.card').hover(
-    function() {
-        $(this).find('.card-text').fadeIn();
+$("#formulario").on("submit", function(e) {
+    e.preventDefault();
 
-    },
-    function() {
-        $(this).find('.card-text').css('display','none');
-    }) */
+    console.log('hi');
+
+    let email = $("#InputEmail").val();
+    let okEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    let okCheckbox = $("#Check").is(":checked");
+
+    if (!okEmail) {
+      alert("Correo inválido.");
+    } else if (!okCheckbox) {
+      alert("Debes aceptar los términos.");
+    } else {
+      alert("Formulario válido. Enviado!");
+    }
+});
