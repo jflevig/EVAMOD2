1bc6614605f9348ae841896fe1c633b138d7c028
jflevig <jfelevi@gmail.com>
2025-06-29 20:42:59 -0400

    Contenido de amenazas agregados

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/* $('.card').hover(
+    function() {
+        $(this).find('.card-text').fadeIn();
+
+    },
+    function() {
+        $(this).find('.card-text').css('display','none');
+    }) */
